c5ad4b17660795fb74d1a519ab522e94f7c02716
Thiago <thiagomlu@hotmail.com.br>
2024-08-05 08:20:18 -0300

    feat: added json examples for swagger requests

--- src/modules/child/dto/child.dto.ts
+++ src/modules/child/dto/child.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsInt, Min, IsNotEmpty } from 'class-validator';
 
 export class ChildDto {
+  @ApiProperty({ example: 'Baby' })
   @IsString()
   @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
+  @ApiProperty({ example: 1 })
   @IsInt()
   @Min(0, { message: 'Age cannot be less than 0' })
   @IsNotEmpty({ message: 'Age is required' })
--- src/modules/father/dto/father.dto.ts
+++ src/modules/father/dto/father.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class FatherDto {
+  @ApiProperty({ example: 'Bob Doe' })
   @IsString()
   @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
+  @ApiProperty({ example: 'bob.doe@example.com' })
   @IsEmail(
     {},
     { message: 'invalid emial format. Example: example@example.com' },
@@ -12,6 +15,7 @@ export class FatherDto {
   @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
+  @ApiProperty({ example: '(99) 999999999' })
   @IsString()
   @IsNotEmpty({ message: 'Phone is required' })
   phone: string;
